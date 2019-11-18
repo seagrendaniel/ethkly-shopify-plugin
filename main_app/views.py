feeb9eaa62ef9daa5d3c4749acafbfda9378f6d1
blackdanamite <seagrendaniel562@gmail.com>
2019-11-18 15:49:47 -0800

    add first url path

---
+++ main_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+# Create your views here.
+
+
+def home(request):
+    return render('Hello World!')
